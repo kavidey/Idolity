727e011e212763011e7dc9aa5575a9b7e53685c3
Kavi Dey <kaviasher@gmail.com>
2022-06-16 21:49:37 -0700

    Initial Commit

---
+++ Idolity/IdolityApp.swift
@@ -0,0 +1,17 @@
+//
+//  IdolityApp.swift
+//  Idolity
+//
+//  Created by Kavi Dey on 6/16/22.
+//
+
+import SwiftUI
+
+@main
+struct IdolityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
